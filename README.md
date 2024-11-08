703c2e7332b1c4786f5435f94e11e2c0f3c618ee
jfortanet <jfortanet@gmail.com>
2024-11-08 16:27:04 +0100

    Update with parcel pre and post-processors and final styling.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # m4257-pac1
-M4.257 - PAC1 Desenvolupament d'una web
+
+Lloc web dedicat al Parc Natural del Penyagolosa, situat a l'interior de la província de Castelló (Espanya). Desenvolupat en el marc de l'assignatura "M4.257 - Eines HTML i CSS I Aula 1", activitat: "PAC1 Desenvolupament d'una web", del màster en "Desenvolupament de llocs i aplicacions web".
+
+Publicat mitjançant la plataforma Netlify.
+
+Estudiant: Javier Garcia Fortanet
+Professor: Mariona Roca Ros
+
+
+
+
---
+++ src/js/slider.js
@@ -0,0 +1,19 @@
+/* Image slider in presentation.html */
+
+let sliderInner = document.querySelector(".slider-inner");
+let images = sliderInner.querySelectorAll("img");
+let currentImage = 0;
+
+/**
+ * Function that automatically changes the image displayed in the slider.
+ * Every 3 seconds the function moves the position of the slider image 
+ * horizontally to show the next image, creating a continuous circular effect.
+ */
+setInterval(() => {
+    let percentage = currentImage * -100; // Displacement percentage.
+    sliderInner.style.transform = "translateX(" + percentage + "%)";
+    currentImage++;
+    if (currentImage > (images.length - 1)) {
+        currentImage = 0;
+    }
+}, 3000);
